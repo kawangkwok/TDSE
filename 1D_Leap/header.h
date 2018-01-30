5087fa9592c07b907f526ca3e2b24c13b91f3d59
Kevin Kwok <33100899+kawangkwok@users.noreply.github.com>
2018-01-30 17:57:49 +0000

    Add files via upload

---
+++ 1D_Leap/header.h
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <math.h>
+#include <iomanip>
+#include <cmath>
+#include <fstream>
+using namespace std;
+
+extern const double dx;
+extern const double dt;
+extern const int m;
+
+void time(int,string[20]);
+void initial(double[201][2]);
+void up(double[201][2], double[201][2]);
+double potential(double);
